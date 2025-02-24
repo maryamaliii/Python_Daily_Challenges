a486dad71f50852297aa4d8f187326192c73d506
maryamaliii <162467024+maryamaliii@users.noreply.github.com>
2025-02-24 19:35:32 +0500

    Python_Daily_Challenges

---
+++ Day_01.py
@@ -0,0 +1,8 @@
+# Daily python challenge Day_01
+
+name:str = "Maryam Anwar"
+message:str = """
+Hello,Python Learners! 
+Today is the first step in our coding journey.
+"""
+print(f"{name} says, {message}")
